4f01befbce6cae49fba494c075d399d0fa153421
Terkwood <metaterkhorn@gmail.com>
2019-03-04 19:52:34 -0500

    Update index and script

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cargo web build --target=wasm32-unknown-unknown --release &&
+  cp target/wasm32-unknown-unknown/release/*.js static/. &&
+  cp target/wasm32-unknown-unknown/release/*.wasm static/. 
