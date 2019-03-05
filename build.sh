4db1547301b4509f6e192b2a63324ffbf2940404
Terkwood <metaterkhorn@gmail.com>
2019-03-04 20:19:46 -0500

    Add wasm-opt run

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 cargo web build --target=wasm32-unknown-unknown --release &&
   cp target/wasm32-unknown-unknown/release/*.js static/. &&
-  cp target/wasm32-unknown-unknown/release/*.wasm static/. 
+  cp target/wasm32-unknown-unknown/release/*.wasm static/. &&
+  wasm-opt -Oz -o static/rust-wasm-pong.wasm static/rust-wasm-pong.wasm
