88762f6c791031f6da570898913b6254430bc65d
Terkwood <metaterkhorn@gmail.com>
2019-03-06 07:48:05 -0500

    simplify and wip

--- src/game.rs
+++ src/game.rs
@@ -16,8 +16,8 @@ impl Game {
      * Renamed from `start` in the original version.
      * Removed the `game` and `cfg` args from the original version.
      */
-    pub fn new(canvas_id: &str) -> Self {
-        let r = Runner::new(canvas_id);
+    pub fn new(front_canvas_id: &str, back_canvas_id: &str) -> Self {
+        let r = Runner::new(front_canvas_id, back_canvas_id);
         Game {
             runner: Box::new(r),
         }
@@ -36,45 +36,45 @@ pub struct Runner {
     fps: u16,
     interval: f32,
     // TODO  dup of front.  Can we remove it?
-    canvas: Box<CanvasElement>,
+    front_canvas: Box<CanvasElement>,
     pub width: i32,
     pub height: i32,
-    front: CanvasElement,
-    front_width: i32,
-    front_height: i32,
-    back: CanvasElement,
-    back_width: i32,
-    back_height: i32,
-    front_2d: CanvasRenderingContext2d,
-    back_2d: CanvasRenderingContext2d,
+    back_canvas: Box<CanvasElement>,
+    front_canvas_2d: CanvasRenderingContext2d,
+    back_canvas_2d: CanvasRenderingContext2d,
 }
 
 impl Runner {
-    pub fn new(canvas_id: &str) -> Runner {
+    pub fn new(front_canvas_id: &str, back_canvas_id: &str) -> Runner {
         let fps = 60;
-        let canvas: CanvasElement = document()
-            .get_element_by_id(canvas_id)
+        let front_canvas: CanvasElement = document()
+            .get_element_by_id(front_canvas_id)
             .unwrap()
             .try_into()
             .unwrap();
-        let canvas_width: i32 = canvas.offset_width();
-        let canvas_height: i32 = canvas.offset_height();
+        let canvas_width: i32 = front_canvas.offset_width();
+        let canvas_height: i32 = front_canvas.offset_height();
+        let back_canvas: CanvasElement = document()
+            .get_element_by_id(back_canvas_id)
+            .unwrap()
+            .try_into()
+            .unwrap();
+        let f2d = front_canvas.get_context().unwrap();
+        let b2d = back_canvas.get_context().unwrap();
         let r = Runner {
             stats: Stats::new(),
             fps: fps,
             interval: 1000.0 / fps as f32,
-            canvas: Box::new(canvas),
+            front_canvas: Box::new(front_canvas),
             width: canvas_width,
             height: canvas_height,
-            front: unimplemented!(),
-            front_width: unimplemented!(),
-            front_height: unimplemented!(),
-            back: unimplemented!(),
-            back_width: unimplemented!(),
-            back_height: unimplemented!(),
-            front_2d: unimplemented!(),
-            back_2d: unimplemented!(),
+            back_canvas: Box::new(back_canvas),
+            front_canvas_2d: f2d,
+            back_canvas_2d: b2d,
         };
+
+        // TODO addEvents (keydown, keyup)
+        unimplemented!();
 
         r
     }
