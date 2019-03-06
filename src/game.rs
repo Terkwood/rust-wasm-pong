5dc7447c1595eda23b42aa95e97d2c3f977c50b2
Terkwood <metaterkhorn@gmail.com>
2019-03-06 06:53:21 -0500

    stubby

--- src/game.rs
+++ src/game.rs
@@ -1,8 +1,13 @@
+use stdweb::traits::*;
+use stdweb::unstable::TryInto;
+use stdweb::web::{document, window, CanvasRenderingContext2d};
+
 use crate::Cfg;
 
 struct Game {
     runner: Box<Runner>,
 }
+
 impl Game {
     /**
      * Create a new instance of the game, exposing methods relating
@@ -23,11 +28,18 @@ pub fn load_images(sources: Vec<String>, callback: Box<(FnOnce(Vec<String>) -> (
 
 pub struct Runner {
     pub cfg: Cfg,
+    pub fps: u16,
+    pub interval: f32,
+    pub canvas: CanvasRenderingContext2d,
     pub width: u32,
     pub height: u32,
 }
 
 impl Runner {
+    pub fn new(id: u32, game: u32, cfg: Cfg) -> Runner {
+        unimplemented!()
+    }
+
     pub fn confirm(&self, _arg: &str) -> bool {
         unimplemented!()
     }
