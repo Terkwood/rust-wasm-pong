ecc58fd3dd38c5002436ca3cb3503a947d6c7aa1
Terkwood <metaterkhorn@gmail.com>
2019-03-11 15:39:21 -0400

    Gzip wasm
    Squashed commit of the following:
    
    commit ed72e157324b886edef59f0918fe8e8a6a2f0fda
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Mon Mar 11 15:37:24 2019 -0400
    
        Try magic
    
    commit 3b0c81635181b5d35641c2e3e667fec5cf85c1a8
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Mon Mar 11 15:33:48 2019 -0400
    
        hack js
    
    commit f937cd04cb14d8a9d0d93a00d876b63ac77fcfa5
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Mon Mar 11 15:30:58 2019 -0400
    
        mime
    
    commit 0ed599c00ee433640c3e03022724c3b5413136f4
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Mon Mar 11 15:29:18 2019 -0400
    
        rename
    
    commit 32beec4bd4229737873d8bbd5ea6555094b45bb5
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Mon Mar 11 15:28:15 2019 -0400
    
        try to zip
    
    commit 70f07f09e3c7ae57df6f381f5983a346a8bd6758
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Mon Mar 11 15:27:59 2019 -0400
    
        remove dead
    
    commit 9789785954d918d984e2eeb391157b1029a42a04
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Mon Mar 11 15:27:43 2019 -0400
    
        move headers file
    
    commit 3b983721b6b25f3f1b1cc5ff9919bc5ea34096a4
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Mon Mar 11 15:18:45 2019 -0400
    
        Try something
    
    commit 59f8975bc458414b6e1b6b90761958c69f58f9c2
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Mon Mar 11 15:10:33 2019 -0400
    
        update details
    
    commit 2a7558ece710778aa5d89c3641bdbc2b9b010f0c
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Mon Mar 11 15:07:25 2019 -0400
    
        update build
    
    commit 192f9c3a0e33725c96900db289fcf545a3c6d81c
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Mon Mar 11 15:07:08 2019 -0400
    
        Try setting netlify headers
    
    commit 980ce89e2a6441f50fb39d6bb6886bb8e43d7c10
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Mon Mar 11 14:55:50 2019 -0400
    
        gzip wasm file during build 🏗

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 cargo web build --target=wasm32-unknown-unknown --release &&
   cp target/wasm32-unknown-unknown/release/*.js static/. &&
   cp target/wasm32-unknown-unknown/release/*.wasm static/. &&
-  wasm-opt -Oz -o static/rust-wasm-pong.wasm static/rust-wasm-pong.wasm
+  wasm-opt -Oz -o static/rust-wasm-pong.wasm static/rust-wasm-pong.wasm &&
+  gzip --force -9 static/rust-wasm-pong.wasm &&
+  mv static/rust-wasm-pong.wasm.gz static/rust-wasm-pong.wasm
