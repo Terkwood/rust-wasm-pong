901fdb2e6c6905689fb1dbb1142c22d7f149cb7e
Terkwood <metaterkhorn@gmail.com>
2019-03-11 20:21:07 -0400

    Use uncompressed js

--- build.sh
+++ build.sh
@@ -5,6 +5,4 @@ cargo web build --target=wasm32-unknown-unknown --release &&
   cp target/wasm32-unknown-unknown/release/*.wasm static/. &&
   wasm-opt -Oz -o static/rust-wasm-pong.wasm static/rust-wasm-pong.wasm &&
   gzip --force -9 static/rust-wasm-pong.wasm &&
-  mv static/rust-wasm-pong.wasm.gz static/rust-wasm-pong.wasm &&
-  gzip --force -9 static/rust-wasm-pong.js &&
-  mv static/rust-wasm-pong.js.gz static/rust-wasm-pong.js
+  mv static/rust-wasm-pong.wasm.gz static/rust-wasm-pong.wasm
