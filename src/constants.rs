af18c876d3c459a24b99109296a496176c422908
Terkwood <metaterkhorn@gmail.com>
2019-03-14 14:40:28 -0400

    Refactor vals and raise menu bottom y
    Squashed commit of the following:
    
    commit 6817767136bc380367f0a1e8dd1d52397f5da7f6
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Thu Mar 14 14:39:17 2019 -0400
    
        build
    
    commit 41279b56e5d0da168e3c63bdd74d152174981d64
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Thu Mar 14 14:38:31 2019 -0400
    
        raise bottom y menu
    
    commit 60c51af9710f07ff1e9eb5596c667e655af00bdd
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Thu Mar 14 14:35:56 2019 -0400
    
        build
    
    commit 992392d65fb8c46445c7b399ed7b2aec05989899
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Thu Mar 14 14:34:26 2019 -0400
    
        refactor vals

--- src/constants.rs
+++ src/constants.rs
@@ -33,10 +33,3 @@ pub const TEXT_COLOR: Color = Color {
     b: 1.0,
     a: 1.0,
 };
-
-pub const WINNER_MSG: &str = "🏆 WINNER 🏆";
-pub const ONE_PLAYER_START_MSG: &str = "press '1' for\n\nsingle player";
-pub const ONE_PLAYER_CONTROLS_MSG: &str = "'q': move up\n\n'a': move down";
-pub const TWO_PLAYER_START_MSG: &str = "press '2' for\n\ndouble player";
-pub const TWO_PLAYER_CONTROLS_MSG: &str = "'p': move up\n\n'l': move down";
-pub const ZERO_PLAYER_START_MSG: &str = "press '0' for\n\nbots-only 🤖";
--- src/menu.rs
+++ src/menu.rs
@@ -3,6 +3,13 @@ use ggez::{graphics, Context};
 use crate::constants::*;
 use crate::player::Player;
 
+const WINNER_MSG: &str = "🏆 WINNER 🏆";
+const ONE_PLAYER_START_MSG: &str = "press '1' for\n\nsingle player";
+const ONE_PLAYER_CONTROLS_MSG: &str = "'q': move up\n\n'a': move down";
+const TWO_PLAYER_START_MSG: &str = "press '2' for\n\ndouble player";
+const TWO_PLAYER_CONTROLS_MSG: &str = "'p': move up\n\n'l': move down";
+const ZERO_PLAYER_START_MSG: &str = "press '0' for\n\nbots-only 🤖";
+
 #[derive(Clone)]
 pub struct Menu {
     player_one_wins: TextBox,
@@ -12,49 +19,65 @@ pub struct Menu {
     two_player_start: TextBox,
     two_player_controls: TextBox,
     zero_player_start: TextBox,
+    additional_instructions: TextBox,
     winner: Option<Player>,
 }
 
 impl Menu {
     pub fn new(game_width: f32, game_height: f32) -> Menu {
-        let left_start_offset: f32 = 0.05 * game_width;
+        let p1_win_x = game_width * 0.18;
+        let p2_win_x = game_width * 0.66;
+        let p1_start_x = 0.05 * game_width;
+        let p1_controls_x = 0.085 * game_width;
+        let controls_y = 6.0 * BLOCK_LENGTH_TO_SCREEN_HEIGHT * game_height;
+
+        let p2_start_x = 0.835 * game_width;
+        let bottom_start_y = game_height - 8.0 * BLOCK_LENGTH_TO_SCREEN_HEIGHT * game_height;
+        let p2_controls_x = 0.80 * game_width;
+        let top_y = 2.0 * BLOCK_LENGTH_TO_SCREEN_HEIGHT * game_height;
+        let win_y = game_height * 0.45;
+
         Menu {
             player_one_wins: TextBox {
-                x: game_width * 0.18,
-                y: game_height * 0.45,
+                x: p1_win_x,
+                y: win_y,
                 msg: WINNER_MSG.to_string(),
             },
             player_two_wins: TextBox {
-                x: game_width * 0.66,
-                y: game_height * 0.45,
+                x: p2_win_x,
+                y: win_y,
                 msg: WINNER_MSG.to_string(),
             },
             one_player_start: TextBox {
-                x: left_start_offset,
-                y: 2.0 * BLOCK_LENGTH_TO_SCREEN_HEIGHT * game_height,
+                x: p1_start_x,
+                y: top_y,
                 msg: ONE_PLAYER_START_MSG.to_string(),
             },
             one_player_controls: TextBox {
-                x: 0.085 * game_width,
-                y: 6.0 * BLOCK_LENGTH_TO_SCREEN_HEIGHT * game_height,
+                x: p1_controls_x,
+                y: controls_y,
                 msg: ONE_PLAYER_CONTROLS_MSG.to_string(),
             },
             two_player_start: TextBox {
-                x: 0.835 * game_width,
-                y: 2.0 * BLOCK_LENGTH_TO_SCREEN_HEIGHT * game_height,
+                x: p2_start_x,
+                y: top_y,
                 msg: TWO_PLAYER_START_MSG.to_string(),
             },
             two_player_controls: TextBox {
-                x: 0.80 * game_width,
-                y: 6.0 * BLOCK_LENGTH_TO_SCREEN_HEIGHT * game_height,
+                x: p2_controls_x,
+                y: controls_y,
                 msg: TWO_PLAYER_CONTROLS_MSG.to_string(),
             },
             zero_player_start: TextBox {
-                x: left_start_offset,
-                y: game_height - 5.0 * BLOCK_LENGTH_TO_SCREEN_HEIGHT * game_height,
+                x: p1_start_x,
+                y: bottom_start_y,
                 msg: ZERO_PLAYER_START_MSG.to_string(),
             },
-
+            additional_instructions: TextBox {
+                x: 0.0,
+                y: 0.0,
+                msg: "".to_string(),
+            },
             winner: None,
         }
     }
